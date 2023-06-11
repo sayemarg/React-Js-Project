557135d839f0e3b469e79fded8081dc8a774450b
SayeMarg <SayeMarg.Developer@gmail.com>
2023-06-11 15:46:21 +0330

    Step 3.1: Cool Table!
    Implement the basic logic of the sortable table

--- react_project/src/pages/TablePage.jsx
+++ react_project/src/pages/TablePage.jsx
@@ -1,23 +1,85 @@
+import tableData from "../constants/table_data.json";
+import { useState } from "react";
+
 function TablePage() {
+	const table_header = [
+		{
+			title: "Full Name",
+			key: "full_name",
+		},
+		{
+			title: "Personnel ID",
+			key: "personnel_id",
+		},
+		{
+			title: "Employeement Date",
+			key: "employment_date",
+		},
+		{
+			title: "Work Experience",
+			key: "work_experience",
+		},
+	];
+
+	const [sortKey, setSortedBy] = useState(null);
+	const [order, setOrder] = useState(null);
+
+	const changeSortedBy = (key) => {
+		if (sortKey !== key) {
+			setSortedBy(key);
+			setOrder("asc");
+			return;
+		}
+
+		if (order === "asc") {
+			setOrder("desc");
+		} else {
+			setSortedBy(null);
+			setOrder(null);
+		}
+	};
+
+	const sortedTableData =
+		sortKey === null
+			? tableData
+			: [...tableData].sort(
+					(data_1, data_2) =>
+						(data_1[sortKey] < data_2[sortKey] ? -1 : 1) *
+						(order === "asc" ? 1 : -1)
+			  );
+
 	return (
 		<>
 			<h1>Table Page</h1>
 
-			<p>
-				Aliquip do culpa elit in nulla cupidatat. Deserunt sunt
-				adipisicing aliqua deserunt reprehenderit do non anim. Ullamco
-				consequat enim proident quis ullamco ullamco ut mollit nulla.
-				Nulla cillum pariatur consequat anim dolor mollit pariatur.
-				Exercitation nulla velit ipsum esse et laborum Lorem. Ipsum ex
-				et fugiat magna consequat et ipsum magna officia aute
-				exercitation deserunt. Proident est reprehenderit id dolor non
-				proident exercitation eu nulla laborum est. Ipsum ut nulla
-				cupidatat fugiat. Reprehenderit quis est consectetur excepteur
-				in ex occaecat dolor consequat esse Lorem consectetur esse
-				ullamco. Minim consectetur nisi eu do minim ad duis est
-				adipisicing in. Enim dolor occaecat elit ullamco consequat
-				officia laborum.
-			</p>
+			<table>
+				<thead>
+					<tr>
+						<th>Row</th>
+
+						{table_header.map((header) => (
+							<th
+								key={header.key}
+								onClick={() => changeSortedBy(header.key)}
+							>
+								{header.title}
+							</th>
+						))}
+					</tr>
+				</thead>
+
+				<tbody>
+					{sortedTableData.map((data, index) => (
+						<tr key={data.id}>
+							<td>{index + 1}</td>
+							<td>{data.full_name}</td>
+							<td>{data.personnel_id}</td>
+							<td>{data.employment_date}</td>
+							<td>{data.work_experience}</td>
+						</tr>
+					))}
+				</tbody>
+			</table>
 		</>
 	);
 }
